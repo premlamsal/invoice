262864f5fef4754fba73b1a30ab3ff8c9ace0686
premlamsal <premlamsal2@gmail.com>
2019-09-19 20:24:35 +0545

    update

--- app/Http/Resources/Invoice.php
+++ app/Http/Resources/Invoice.php
@@ -14,19 +14,19 @@ class Invoice extends JsonResource
      */
     public function toArray($request)
     {
-         return parent::toArray($request);// will sent all data 
-        //  return[
-        //     'id'=>$this->id,
-        //     'title'=> $this->title,
-        //     'customer_name'=> $this->customer_name,
-        //     'invoice_date'=> $this->invoice_date,
-        //     'due_date'=> $this->due_date,
-        //     'discount'=> $this->discount,
-        //     'grand_total'=> $this->grand_total,
-        //     'invoice_no'=> $this->invoice_no,
-        //     'status'=>$this->status,
-        //     'created_at'=>$this->created_at->diffForHumans(),
-        // ];
+         // return parent::toArray($request);// will sent all data 
+         return[
+            'id'=>$this->id,
+            'title'=> $this->title,
+            'customer_name'=> $this->customer_name,
+            'invoice_date'=> $this->invoice_date,
+            'due_date'=> $this->due_date,
+            'discount'=> $this->discount,
+            'grand_total'=> $this->grand_total,
+            'invoice_no'=> $this->invoice_no,
+            'status'=>$this->status,
+            'updated_at'=>$this->created_at->diffForHumans(),
+        ];
 
 
     }
